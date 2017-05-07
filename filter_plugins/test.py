9e360d77268520086a6b5b1ac71249e99be20dc7
Xu YinYing <sjtuxuyinying@gmail.com>
2017-05-08 07:54:28 +0800

    add esxcli hostname change

--- filter_plugins/test.py
+++ filter_plugins/test.py
@@ -1,5 +1,12 @@
 def ipsplit(ip):
-    return str(ip).split('.')
+    lst = str(ip).split('.')
+    if len(lst[2]) < 3:
+        lst[2] = "0" + lst[2]
+    if len(lst[3]) < 3:
+        lst[3] = "0" + lst[3]
+
+    return lst
+
 
 class FilterModule(object):
     '''
